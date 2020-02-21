db229367c42d636b5d9cc9e78b7e283aec29abf5
Jared Hanson <jred0011@gmail.com>
2020-02-21 15:12:55 -0500

    Initial commit

---
+++ client/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// Bootstrap and dependencies
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import PortalVue from 'portal-vue'
+Vue.use(BootstrapVue)
+Vue.use(PortalVue)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
